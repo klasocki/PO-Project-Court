c48a00feee6d4fbd00ed2344616ffe9520054935
klasocki <karolasocki@gmail.com>
2018-11-23 20:34:58 +0100

    Add a JSON hello world, testing javax.json

--- Src/Console/Main.java
+++ Src/Console/Main.java
@@ -1,7 +1,29 @@
 package Console;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String filename = "json/judgments-348.json";
+        try (FileReader reader = new FileReader(filename)) {
+            JsonReader jsonReader = Json.createReader(reader);
+            JsonObject object = jsonReader.readObject();
+            System.out.println("Hello world");
+            System.out.println(object.toString());
+            jsonReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Plik: " + filename + " nie istnieje");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Problem z otwarciem pliku: " + filename );
+            System.out.println(e.getMessage());
+        }
+
     }
+
 }
