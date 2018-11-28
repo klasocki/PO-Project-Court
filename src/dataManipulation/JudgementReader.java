e70bed601b6602c3f5e129b6ebdbc3431e25d4fb
klasocki <karolasocki@gmail.com>
2018-11-28 16:15:14 +0100

    Add readAll method to JudgementReader It can 'read all judgements from a file or from a list of files

--- src/dataManipulation/JudgementReader.java
+++ src/dataManipulation/JudgementReader.java
@@ -7,15 +7,42 @@ import court.Judgement;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class JudgementReader {
     public Judgement readSingle(String filePath, int index) throws IOException, IndexOutOfBoundsException{
+        var gson = new Gson();
+        var jsonArray = readJsonArray(filePath);
+        Judgement result = gson.fromJson(jsonArray.get(index), Judgement.class);
+        return result;
+}
+
+    public Map<Integer, Judgement> readAll(String filePath) throws IOException{
+        var gson = new Gson();
+        var jsonArray = readJsonArray(filePath);
+        var result = new LinkedHashMap<Integer, Judgement>();
+        for (var jsonElement : jsonArray) {
+            var judgement = gson.fromJson(jsonElement, Judgement.class);
+            result.put(judgement.getId(), judgement);
+        }
+        return result;
+    }
+
+    public Map<Integer, Judgement> readAll(String[] files) throws IOException {
+        var result = new LinkedHashMap<Integer, Judgement>();
+        for (String filePath : files) {
+            result.putAll(readAll(filePath));
+        }
+        return result;
+    }
+
+    private JsonArray readJsonArray(String filePath) throws IOException {
         try (FileReader reader = new FileReader(filePath)) {
             var gson = new Gson();
             var jsonObject = gson.fromJson(reader, JsonObject.class);
             var jsonArray = gson.fromJson(jsonObject.getAsJsonArray("items"), JsonArray.class);
-            Judgement result = gson.fromJson(jsonArray.get(index), Judgement.class);
-            return result;
+            return jsonArray;
         }
     }
 }
