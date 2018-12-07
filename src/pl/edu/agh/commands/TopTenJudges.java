80de7c838e9b27fabebf04420f6fc731abcf707f
klasocki <karolasocki@gmail.com>
2018-12-07 13:19:17 +0100

    Add TopTenJudges command, manually tested

--- src/pl/edu/agh/commands/TopTenJudges.java
+++ src/pl/edu/agh/commands/TopTenJudges.java
@@ -2,9 +2,36 @@ package pl.edu.agh.commands;
 
 import pl.edu.agh.model.Judgement;
 
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.reverseOrder;
 
 public class TopTenJudges {
     private Map<String, Judgement> judgements;
+
+    public TopTenJudges(Map<String, Judgement> judgements) {
+        this.judgements = judgements;
+    }
+
+    public String getTopTen() {
+        var judges = new HashMap<String, Integer>();
+        for (var judgement : judgements.values()) {
+            for (var judge : judgement.getJudges()) {
+                // This adds pair (name, 1) to the judges map is no entry is present,
+                // otherwise increments the existing counter
+                judges.merge(judge.getName(), 1, Integer::sum);
+            }
+        }
+        //sorting map by number of judgments and taking first 10 values
+        Map<String, Integer> sortedMap = judges.entrySet().stream().sorted(reverseOrder(Map.Entry.comparingByValue())).limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        //building return string (without StringBuilder, cause its guaranteed to run 10 times)
+        String result = "";
+        for (var entry : sortedMap.entrySet()) {
+            result+=entry.getKey() + " - " + entry.getValue() +"\n";
+        }
+        return result;
+    }
 
 }
--- test/pl/edu/agh/TestFileReader.java
+++ test/pl/edu/agh/TestFileReader.java
@@ -5,12 +5,19 @@ import pl.edu.agh.model.Judgement;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class TestFileReader {
     public static Map<String, Judgement> read() throws IOException {
         File testFile = new File("test/pl/edu/agh/dataExtraction/testJudgement.json");
+        return read(testFile);
+    }
+
+    public static Map<String, Judgement> read(File testFile) throws IOException {
         var reader = new JudgmentReader();
         return reader.readAll(testFile);
     }
+
+
 }
