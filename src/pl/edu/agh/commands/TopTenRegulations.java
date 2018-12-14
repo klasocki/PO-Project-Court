0edacce64f11957dcc2817017a5bdd7303d49765
klasocki <karolasocki@gmail.com>
2018-12-14 16:55:11 +0100

    Add TopTenRegulations command

--- src/pl/edu/agh/commands/TopTenRegulations.java
+++ src/pl/edu/agh/commands/TopTenRegulations.java
@@ -2,9 +2,30 @@ package pl.edu.agh.commands;
 
 import pl.edu.agh.model.Judgement;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TopTenRegulations {
     private Map<String, Judgement> judgements;
 
+    public TopTenRegulations(Map<String, Judgement> judgements) {
+        this.judgements = judgements;
+    }
+
+    public String getTopTen() {
+        var regulationsJudgementCount = new HashMap<String, Integer>();
+        var mapUtils = new MapUtils<String, Integer>(); //helper object
+        for (var judgement : judgements.values()) {
+            for (var regulation : judgement.getReferencedRegulations()) {
+                mapUtils.incrValue(regulationsJudgementCount, regulation.getJournalTitle());
+            }
+        }
+        Map<String, Integer> sortedMap = mapUtils.getTopValues(regulationsJudgementCount, 10);
+        //building return string (without StringBuilder, cause its guaranteed to run 10 times)
+        String result = "";
+        for (var entry : sortedMap.entrySet()) {
+            result+=entry.getKey() + " - " + entry.getValue() +"\n";
+        }
+        return result;
+    }
 }
---
+++ test/pl/edu/agh/commands/TopTenRegulationsTest.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.commands;
+
+import org.junit.jupiter.api.Test;
+import pl.edu.agh.dataExtraction.FileLister;
+import pl.edu.agh.dataExtraction.JudgmentReader;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopTenRegulationsTest {
+
+    @Test
+    void getTopTen() throws IOException {
+        var reader = new JudgmentReader();
+        var judgments = reader.readAll(FileLister.listFiles(new File("jsonData"), ".json"));
+        var command = new TopTenRegulations(judgments);
+        System.out.println(command.getTopTen());
+    }
+
+}
