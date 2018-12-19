51034113875c444b9a28b54c02d9c255fa33093f
klasocki <karolasocki@gmail.com>
2018-12-19 12:11:19 +0100

    improve FileLister to list all files including subdirectories

--- src/pl/edu/agh/dataExtraction/FileLister.java
+++ src/pl/edu/agh/dataExtraction/FileLister.java
@@ -1,12 +1,22 @@
 package pl.edu.agh.dataExtraction;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileLister {
-    public static File[] listFiles(File folder, String endingWith) throws IllegalArgumentException {
-        File[] filesFiltered = folder.listFiles((file, s) -> s.endsWith(endingWith));
-        if (filesFiltered == null || filesFiltered.length == 0) {
+    public static File[] listFiles(File folder, String endingWith) throws IllegalArgumentException, IOException {
+
+        File[] filesFiltered = Files.walk(Paths.get(folder.getPath()))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .filter(f -> f.getName().endsWith(endingWith))
+                .toArray(File[]::new);
+
+        if (filesFiltered.length == 0) {
             throw new IllegalArgumentException("No files in directory " + folder.getPath() + " end with: " + endingWith);
         }
         return filesFiltered;
